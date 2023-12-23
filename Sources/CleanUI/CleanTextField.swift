0fd0497adf70610279184055c9d1d231b4fc153b
Alexander Loewe <loewea2000@gmail.com>
2023-12-23 15:53:28 +0100

    added outline and symbol textfield

--- Sources/CleanUI/CleanTextField.swift
+++ Sources/CleanUI/CleanTextField.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 /** The different types of TextFields
  */
-public enum CleanTextFieldStyle: CaseIterable {
+public enum CleanTextFieldStyle {
+    
     case standard
     case line
     case outline
+    case symbol(sfSymbol: String)
 }
 
 public struct CleanTextField: View {
+    
+    @Environment(\.colorScheme) private var colorScheme
     
     @Binding var text : String
     @FocusState private var isFocused : Bool
@@ -46,22 +50,57 @@ public struct CleanTextField: View {
             case .line:
                 LineTextField()
             case .outline:
-                TextField(placeholder, text: $text, axis: .vertical)
+                TextField(isFocused ? "" : placeholder, text: $text, axis: .vertical)
+                    .textFieldMod(keyboardType: keyboard, isFocused: _isFocused)
+                    .padding()
+                    .background {
+                        if isFocused || !text.isEmpty {
+                            ZStack(alignment: .topLeading) {
+                                RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                                    .fill(.clear)
+                                    .stroke(color, lineWidth: 2.0)
+                                Text(placeholder)
+                                    .padding(.horizontal)
+                                    .background(Color.white)
+                                    .offset(x: 20.0, y: -11.0)
+                                    .foregroundStyle(color)
+                            }
+                        }
+                        else {
+                            RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                                .fill(.clear)
+                                .stroke(.secondary, lineWidth: 2.0)
+                        }
+                    }
+            case .symbol(sfSymbol: let sfSymbol):
+                SfSymbolTextField(sfSymbol)
             }
         }
     }
 }
 
 #Preview {
-    VStack(alignment: .center, spacing: 30) {
-        ForEach(CleanTextFieldStyle.allCases, id: \.self) { style in
-            CleanTextField(text: .constant(""),
-                           placeholder: "Email",
-                           style: style)
-                
-        }
+    
+    VStack(spacing: 40) {
+        CleanTextField(text: .constant(""),
+                       placeholder: "Email",
+                       color: .mint,
+                       style: .standard)
+        CleanTextField(text: .constant(""),
+                       placeholder: "Email",
+                       color: .mint,
+                       style: .line)
+        CleanTextField(text: .constant(""),
+                       placeholder: "Email",
+                       color: .mint,
+                       style: .outline)
+        CleanTextField(text: .constant(""),
+                       placeholder: "Email",
+                       color: .mint,
+                       style: .symbol(sfSymbol: "envelope.fill"))
     }
     .padding()
+    .preferredColorScheme(.light)
     
 }
 
@@ -130,6 +169,28 @@ private extension CleanTextField {
                     .frame(height: (isFocused || !text.isEmpty) ? 5 : 2)
                     .overlay(color)
                     .offset(y: -0.4)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func SfSymbolTextField(_ symbol : String) -> some View {
+        VStack(alignment: .leading) {
+            Text(placeholder)
+                .padding(.leading, 10)
+            HStack {
+                Image(systemName: symbol)
+                    .foregroundStyle(color)
+                TextField(placeholder, text: $text, axis: .vertical)
+                    .textFieldMod(keyboardType: keyboard, isFocused: _isFocused)
+                
+            }
+            .padding()
+            .background {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(isFocused ? color.opacity(0.08) : .clear)
+                    .stroke(isFocused ? color : .secondary,
+                            lineWidth: 2.0)
             }
         }
     }
